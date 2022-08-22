da722e68cd2407415680ae4d8c777127490b9c60
Jiaming Guo <j23guo@gmail.com>
2022-08-21 21:30:08 -0400

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Jiaming Guo on 2022-08-21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
