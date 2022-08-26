efb156359a72b1b12e1865306dfe3c2ce71c0881
Jiaming Guo <j23guo@gmail.com>
2022-08-26 11:10:56 -0400

    supported placing order

--- CupcakeCorner/Views/CheckoutView.swift
+++ CupcakeCorner/Views/CheckoutView.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct CheckoutView: View {
     @ObservedObject var order: Order
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
     
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity) \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed.")
+        }
+    }
     
     var body: some View {
         ScrollView {
@@ -25,7 +45,9 @@ struct CheckoutView: View {
                 Text("Your total is \(order.cost, format: .currency(code: Locale.current.currencyCode ?? "USD"))")
                     .font(.title)
                 Button(action: {
-                    
+                    Task {
+                        await placeOrder()
+                    }
                 }, label: {
                     Text("Place Order")
                         .padding(.horizontal)
@@ -33,6 +55,11 @@ struct CheckoutView: View {
                 .buttonStyle(.borderedProminent)
                 .padding()
             }
+        }
+        .alert("Thank you", isPresented: $showingConfirmation) {
+            Button("OK") { }
+        } message: {
+            Text(confirmationMessage)
         }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
