2406f4688cea5cb488f02ecaf7472d315a1694c3
Jiaming Guo <j23guo@gmail.com>
2022-08-29 16:41:50 -0400

    added network error alert and improved address validation

--- CupcakeCornerTests/CupcakeCornerTests.swift
+++ CupcakeCornerTests/CupcakeCornerTests.swift
@@ -32,5 +32,18 @@ class CupcakeCornerTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
+    func testOrderHasValidAddress() throws {
+        let order = Order()
+        order.streetAddress = ""
+        order.city = ""
+        order.postal = ""
+        order.name = ""
+        XCTAssertFalse(order.hasValidAddress)
+        order.streetAddress = "  "
+        order.city = "  "
+        order.postal = "  "
+        order.name = "  "
+        XCTAssertFalse(order.hasValidAddress)
+    }
 }
